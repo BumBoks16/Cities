9778c243d5aed0d261512170f1bd2dc82d81fc21
Dmitriy <FrankieFix@bk.ru>
2022-10-17 23:05:59 +0300

    Create constructor for game class

--- Game.py
+++ Game.py
@@ -10,4 +10,35 @@ class Game:
     bad_chars = ['й', 'ы', 'ь']
     used_towns = []
 
+    def __init__(self):
+        self.player_turn = bool(random.randint(0, 1))
+        self.end_game_state = False
+        self.player_win = False
+        self.current_letter = ""
+        self.towns = self.get_list_of_towns()["towns"]
+        self.towns = set(self.towns)
+        self.towns = list(self.towns)
+        self.original_towns = self.get_list_of_towns()["towns"]
+        self.original_towns = set(self.towns)
+        self.original_towns = list(self.towns)
+
+    def set_player_win(self):
+        self.player_win = True
+
+    def get_player_win(self):
+        if (self.player_win):
+            return True
+        else:
+            return False
+
+    @staticmethod
+    def get_list_of_towns():
+        target_filename = "towns.txt"
+        with open(target_filename, "r", encoding='utf-8') as file:
+            text = file.read()
+            data = json.loads(text)
+        return data
+
+    
+
     
